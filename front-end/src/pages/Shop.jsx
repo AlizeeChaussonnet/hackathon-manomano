e0c57184bae34cbe6a7675971a447e5eb75be37e
Nawfel Bouhadjar <nawfel.bouhadjar@gmail.com>
2022-01-20 00:09:37 +0100

    seller instruction

--- front-end/src/pages/Shop.jsx
+++ front-end/src/pages/Shop.jsx
@@ -3,201 +3,224 @@ import React from "react";
 const Shop = () => {
   return (
     <>
-      <div class="flex justify-center my-6">
-      <img
-              className="w-2/3 h-2/3 object-cover rounded-lg overflow-hidden"
-              src="https://images.unsplash.com/photo-1616486788371-62d930495c44?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80"
-            />
-        <div class="flex flex-col w-full p-8 text-gray-800 bg-white shadow-lg pin-r pin-y md:w-4/5 lg:w-4/5">
-          <div class="flex-1">
-            <table class="w-full text-sm lg:text-base" cellspacing="0">
+      <div className="flex justify-center my-6">
+        <img
+          className="w-2/3 h-2/3 object-cover rounded-lg overflow-hidden"
+          src="https://images.unsplash.com/photo-1616486788371-62d930495c44?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80"
+        />
+        <div className="flex flex-col w-full p-8 text-gray-800 bg-white shadow-lg pin-r pin-y md:w-4/5 lg:w-4/5">
+          <div className="flex-1">
+            <table className="w-full text-sm lg:text-base" cellspacing="0">
               <thead>
-                <tr class="h-12 uppercase">
-                  <th class="hidden md:table-cell"></th>
-                  <th class="text-left">Product</th>
-                  <th class="lg:text-right text-left pl-5 lg:pl-0">
-                    <span class="lg:hidden" title="Quantity">
+                <tr className="h-12 uppercase">
+                  <th className="hidden md:table-cell"></th>
+                  <th className="text-left">Product</th>
+                  <th className="lg:text-right text-left pl-5 lg:pl-0">
+                    <span className="lg:hidden" title="Quantity">
                       Qtd
                     </span>
-                    <span class="hidden lg:inline">Quantity</span>
+                    <span className="hidden lg:inline">Quantity</span>
                   </th>
-                  <th class="hidden text-right md:table-cell">Unit price</th>
-                  <th class="text-right">Total price</th>
+                  <th className="hidden text-right md:table-cell">
+                    Unit price
+                  </th>
+                  <th className="text-right">Total price</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td class="hidden pb-4 md:table-cell">
+                  <td className="hidden pb-4 md:table-cell">
                     <a href="#">
                       <img
                         src="https://limg.app/i/Calm-Cormorant-Catholic-Pinball-Blaster-yM4oub.jpeg"
-                        class="w-20 rounded"
+                        className="w-20 rounded"
                         alt="Thumbnail"
                       />
                     </a>
                   </td>
                   <td>
                     <a href="#">
-                      <p class="mb-2 md:ml-4">Earphone</p>
+                      <p className="mb-2 md:ml-4">Earphone</p>
                       <form action="" method="POST">
-                        <button type="submit" class="text-gray-700 md:ml-4">
+                        <button type="submit" className="text-gray-700 md:ml-4">
                           <small>(Remove item)</small>
                         </button>
                       </form>
                     </a>
                   </td>
-                  <td class="justify-center md:justify-end md:flex mt-6">
-                    <div class="w-20 h-10">
-                      <div class="relative flex flex-row w-full h-8">
+                  <td className="justify-center md:justify-end md:flex mt-6">
+                    <div className="w-20 h-10">
+                      <div className="relative flex flex-row w-full h-8">
                         <input
                           type="number"
                           value="2"
-                          class="w-full font-semibold text-center text-gray-700 bg-gray-200 outline-none focus:outline-none hover:text-black focus:text-black"
+                          className="w-full font-semibold text-center text-gray-700 bg-gray-200 outline-none focus:outline-none hover:text-black focus:text-black"
                         />
                       </div>
                     </div>
                   </td>
-                  <td class="hidden text-right md:table-cell">
-                    <span class="text-sm lg:text-base font-medium">10.00€</span>
+                  <td className="hidden text-right md:table-cell">
+                    <span className="text-sm lg:text-base font-medium">
+                      10.00€
+                    </span>
                   </td>
-                  <td class="text-right">
-                    <span class="text-sm lg:text-base font-medium">20.00€</span>
+                  <td className="text-right">
+                    <span className="text-sm lg:text-base font-medium">
+                      20.00€
+                    </span>
                   </td>
                 </tr>
                 <tr>
-                  <td class="hidden pb-4 md:table-cell">
+                  <td className="hidden pb-4 md:table-cell">
                     <a href="#">
                       <img
                         src="https://limg.app/i/Cute-Constrictor-Super-Sexy-Military-Enforcer-W7mvBp.png"
-                        class="w-20 rounded"
+                        className="w-20 rounded"
                         alt="Thumbnail"
                       />
                     </a>
                   </td>
                   <td>
-                    <p class="mb-2 md:ml-4">Tesla Model 3</p>
+                    <p className="mb-2 md:ml-4">Tesla Model 3</p>
                     <form action="" method="POST">
-                      <button type="submit" class="text-gray-700 md:ml-4">
+                      <button type="submit" className="text-gray-700 md:ml-4">
                         <small>(Remove item)</small>
                       </button>
                     </form>
                   </td>
-                  <td class="justify-center md:justify-end md:flex md:mt-4">
-                    <div class="w-20 h-10">
-                      <div class="relative flex flex-row w-full h-8">
+                  <td className="justify-center md:justify-end md:flex md:mt-4">
+                    <div className="w-20 h-10">
+                      <div className="relative flex flex-row w-full h-8">
                         <input
                           type="number"
                           value="3"
-                          class="w-full font-semibold text-center text-gray-700 bg-gray-200 outline-none focus:outline-none hover:text-black focus:text-black"
+                          className="w-full font-semibold text-center text-gray-700 bg-gray-200 outline-none focus:outline-none hover:text-black focus:text-black"
                         />
                       </div>
                     </div>
                   </td>
-                  <td class="hidden text-right md:table-cell">
-                    <span class="text-sm lg:text-base font-medium">
+                  <td className="hidden text-right md:table-cell">
+                    <span className="text-sm lg:text-base font-medium">
                       49,600.01€
                     </span>
                   </td>
-                  <td class="text-right">
-                    <span class="text-sm lg:text-base font-medium">
+                  <td className="text-right">
+                    <span className="text-sm lg:text-base font-medium">
                       148,800.03€
                     </span>
                   </td>
                 </tr>
                 <tr>
-                  <td class="hidden pb-4 md:table-cell">
+                  <td className="hidden pb-4 md:table-cell">
                     <a href="#">
                       <img
                         src="https://limg.app/i/Successful-Spider-Biblical-Mutant---Total-War-lKoE7D.jpeg"
-                        class="w-20 rounded"
+                        className="w-20 rounded"
                         alt="Thumbnail"
                       />
                     </a>
                   </td>
                   <td>
-                    <p class="mb-2 md:ml-4">Bic 4 colour pen</p>
+                    <p className="mb-2 md:ml-4">Bic 4 colour pen</p>
                     <form action="" method="POST">
-                      <button type="submit" class="text-gray-700 md:ml-4">
+                      <button type="submit" className="text-gray-700 md:ml-4">
                         <small>(Remove item)</small>
                       </button>
                     </form>
                   </td>
-                  <td class="justify-center md:justify-end md:flex md:mt-8">
-                    <div class="w-20 h-10">
-                      <div class="relative flex flex-row w-full h-8">
+                  <td className="justify-center md:justify-end md:flex md:mt-8">
+                    <div className="w-20 h-10">
+                      <div className="relative flex flex-row w-full h-8">
                         <input
                           type="number"
                           value="5"
-                          class="w-full font-semibold text-center text-gray-700 bg-gray-200 outline-none focus:outline-none hover:text-black focus:text-black"
+                          className="w-full font-semibold text-center text-gray-700 bg-gray-200 outline-none focus:outline-none hover:text-black focus:text-black"
                         />
                       </div>
                     </div>
                   </td>
-                  <td class="hidden text-right md:table-cell">
-                    <span class="text-sm lg:text-base font-medium">1.50€</span>
+                  <td className="hidden text-right md:table-cell">
+                    <span className="text-sm lg:text-base font-medium">
+                      1.50€
+                    </span>
                   </td>
-                  <td class="text-right">
-                    <span class="text-sm lg:text-base font-medium">7.50€</span>
+                  <td className="text-right">
+                    <span className="text-sm lg:text-base font-medium">
+                      7.50€
+                    </span>
                   </td>
                 </tr>
               </tbody>
             </table>
-            <hr class="pb-6 mt-6" />
-            <div class="my-4 mt-6 -mx-2 lg:flex">
-              <div class="lg:px-2 lg:w-1/2"></div>
-            </div>
-            <div class="lg:px-2 lg:w-1/2">
-              <div class="p-4 bg-gray-100 rounded-full">
-                <h1 class="ml-2 font-bold uppercase">Order Details</h1>
-              </div>
-              <div class="p-4">
-                <p class="mb-6 italic">
-                  Shipping and additionnal costs are calculated based on values
-                  you have entered
-                </p>
-                <div class="flex justify-between border-b">
-                  <div class="lg:px-4 lg:py-2 m-2 text-lg lg:text-xl font-bold text-center text-gray-800">
-                    Subtotal
+            <hr className="pb-6 mt-6" />
+            <div className="my-4 mt-6 -mx-2 lg:flex">
+              <div className="lg:px-2 lg:w-1/2">
+                <div className="p-4 bg-gray-100 rounded-full">
+                  <h1 className="ml-2 font-bold uppercase">Order Details</h1>
+                </div>
+                <div className="p-4">
+                  <p className="mb-6 italic">
+                    Shipping and additionnal costs are calculated based on
+                    values you have entered
+                  </p>
+                  <div className="flex justify-between border-b">
+                    <div className="lg:px-4 lg:py-2 m-2 text-lg lg:text-xl font-bold text-center text-gray-800">
+                      Subtotal
+                    </div>
+                    <div className="lg:px-4 lg:py-2 m-2 lg:text-lg font-bold text-center text-gray-900">
+                      148,827.53€
+                    </div>
                   </div>
-                  <div class="lg:px-4 lg:py-2 m-2 lg:text-lg font-bold text-center text-gray-900">
-                    148,827.53€
+                </div>
+                <div className="flex justify-between pt-4 border-b">
+                  <div className="lg:px-4 lg:py-2 m-2 text-lg lg:text-xl font-bold text-center text-gray-800">
+                    Tax
+                  </div>
+                  <div className="lg:px-4 lg:py-2 m-2 lg:text-lg font-bold text-center text-gray-900">
+                    2,976.55€
                   </div>
                 </div>
+                <div className="flex justify-between pt-4 border-b">
+                  <div className="lg:px-4 lg:py-2 m-2 text-lg lg:text-xl font-bold text-center text-gray-800">
+                    Total
+                  </div>
+                  <div className="lg:px-4 lg:py-2 m-2 lg:text-lg font-bold text-center text-gray-900">
+                    17,859.3€
+                  </div>
+                </div>
+                <a href="#">
+                  <button className="flex justify-center w-full px-10 py-3 mt-6 font-medium text-white uppercase bg-gray-800 rounded-full shadow item-center hover:bg-gray-700 focus:shadow-outline focus:outline-none">
+                    <svg
+                      aria-hidden="true"
+                      data-prefix="far"
+                      data-icon="credit-card"
+                      className="w-8"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 576 512"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M527.9 32H48.1C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48.1 48h479.8c26.6 0 48.1-21.5 48.1-48V80c0-26.5-21.5-48-48.1-48zM54.1 80h467.8c3.3 0 6 2.7 6 6v42H48.1V86c0-3.3 2.7-6 6-6zm467.8 352H54.1c-3.3 0-6-2.7-6-6V256h479.8v170c0 3.3-2.7 6-6 6zM192 332v40c0 6.6-5.4 12-12 12h-72c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h72c6.6 0 12 5.4 12 12zm192 0v40c0 6.6-5.4 12-12 12H236c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h136c6.6 0 12 5.4 12 12z"
+                      />
+                    </svg>
+                    <span className="ml-2 mt-5px">Procceed to checkout</span>
+                  </button>
+                </a>
               </div>
-              <div class="flex justify-between pt-4 border-b">
-                <div class="lg:px-4 lg:py-2 m-2 text-lg lg:text-xl font-bold text-center text-gray-800">
-                  Tax
+              <div className="lg:px-2 lg:w-1/2">
+                <div className="p-4 bg-gray-100 rounded-full">
+                  <h1 className="ml-2 font-bold uppercase">
+                    Instruction for seller
+                  </h1>
                 </div>
-                <div class="lg:px-4 lg:py-2 m-2 lg:text-lg font-bold text-center text-gray-900">
-                  2,976.55€
-                </div>
-              </div>
-              <div class="flex justify-between pt-4 border-b">
-                <div class="lg:px-4 lg:py-2 m-2 text-lg lg:text-xl font-bold text-center text-gray-800">
-                  Total
-                </div>
-                <div class="lg:px-4 lg:py-2 m-2 lg:text-lg font-bold text-center text-gray-900">
-                  17,859.3€
+                <div className="p-4">
+                  <p className="mb-4 italic">
+                    If you have some information for the seller you can leave
+                    them in the box below
+                  </p>
+                  <textarea className="w-full h-24 p-2 bg-gray-100 rounded"></textarea>
                 </div>
               </div>
-              <a href="#">
-                <button class="flex justify-center w-full px-10 py-3 mt-6 font-medium text-white uppercase bg-gray-800 rounded-full shadow item-center hover:bg-gray-700 focus:shadow-outline focus:outline-none">
-                  <svg
-                    aria-hidden="true"
-                    data-prefix="far"
-                    data-icon="credit-card"
-                    class="w-8"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 576 512"
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M527.9 32H48.1C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48.1 48h479.8c26.6 0 48.1-21.5 48.1-48V80c0-26.5-21.5-48-48.1-48zM54.1 80h467.8c3.3 0 6 2.7 6 6v42H48.1V86c0-3.3 2.7-6 6-6zm467.8 352H54.1c-3.3 0-6-2.7-6-6V256h479.8v170c0 3.3-2.7 6-6 6zM192 332v40c0 6.6-5.4 12-12 12h-72c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h72c6.6 0 12 5.4 12 12zm192 0v40c0 6.6-5.4 12-12 12H236c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h136c6.6 0 12 5.4 12 12z"
-                    />
-                  </svg>
-                  <span class="ml-2 mt-5px">Procceed to checkout</span>
-                </button>
-              </a>
             </div>
           </div>
         </div>
